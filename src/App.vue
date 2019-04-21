183511480a822db3ae4e813eae24181d44a1747c
Will <will.jenkins675@gmail.com>
2019-04-21 13:08:09 +0100

    Add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline">
-        <v-btn flat class="font-weight-light">sign in / register</v-btn>
-        <span>fabble</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat>search</v-btn>
-      <v-btn flat>basket</v-btn>
-    </v-toolbar>
+    <Header/>
 
     <v-content>
-      <router-view></router-view>
+      <router-view/>
     </v-content>
+
+    <Footer/>
   </v-app>
 </template>
 
 <script>
-// import HelloFabble from './components/HelloFabble'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 export default {
-  name: 'App',
-  // components: {
-  //   HelloFabble
-  // },
-  data () {
+  name: "App",
+  data() {
     return {
-      //
-    }
+    };
+  },
+  components: {
+    Header,
+    Footer
   }
-}
+};
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer height="auto">
+    <v-layout justify-center row wrap>
+      <v-btn v-for="link in links" :key="link" flat round>{{ link }}</v-btn>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    links: ["home", "who we are", "what we do", "charities we support", "talk to us"]
+  })
+};
+</script>
